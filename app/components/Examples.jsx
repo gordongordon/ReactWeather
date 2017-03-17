27c9bfa11dc2c350d68077e2e26047f9f7378db9
Gordon <3225297@gmail.com>
2017-03-17 21:18:12 +0800

    Add grid

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
-  return <h3>Examples Component</h3>;
+  return (
+    <div>
+        <h1 className="text-center">Examples Component</h1>
+        <p>Here are a few Examples location to try out;</p>
+        <ol>
+          <li>
+            <Link to='/?location=Hong Kong'>Hong Kong</Link>
+          </li>
+          <li>
+             <Link to='/?location=Rio'>
+                 Rio
+             </Link>
+          </li>
+        </ol>
+    </div>
+  );
 };
 
 module.exports = Examples;
